e42bba4db225bc32ec0f9f087003c6b419234716
CharithaMadhumal <charithamadhumal174@gmail.com>
2024-08-15 23:44:49 +0530

    second commit

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { addFood } from "../controlers/foodController.js"
+import { addFood,listFood,removeFood } from "../controlers/foodController.js"
 
 import multer from "multer"
 
@@ -10,14 +10,14 @@ const foodRouter = express.Router();
 const storage = multer.diskStorage({
     destination:"uploads",
     filename:(req,file,cb)=>{
-        return cb(null, `${date.now()}${file.originalname}`)
+        return cb(null, `${Date.now()}${file.originalname}`)
     }
 })
 
 const upload = multer({storage:storage})
 
 foodRouter.post("/add",upload.single("image"),addFood)
-
-
+foodRouter.get("/list",listFood)
+foodRouter.post("/remove",removeFood);
 
 export default foodRouter;
